7a07dd76532a3bea8996fc0ee42beae9bd74ef9c
jedspears <jed.spears7@gmail.com>
2019-06-04 14:21:28 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+
+
+const codeValues = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let index = 0;
+
 function init() {
   // your code here
+  const main = document.body;
+  main .addEventListener('keydown', function(e) {
+    // const keyb = KeyboardEvent()
+    for (let i = 0, j = codeValues.length; i < j; i ++) {
+      if(codeValues[i] === e.key) {
+        index++
+        i++
+      } else {
+        index = 0;
+        i = 0;
+      }
+    }
+    alert('Nice!');
+  })
 }
