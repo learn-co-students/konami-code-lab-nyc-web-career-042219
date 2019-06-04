2bcf0445fdbffa4fde632675b89925c1d77226dd
Jennifer Oh <ohjennifer.90@gmail.com>
2019-06-03 20:53:44 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,17 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+  document.body.addEventListener('keydown', function(e){
+    const key = e.key;
+    if (key === codes[index]){
+      ++index;
+      if (index === codes.length){
+        window.alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
